21ddc05c64e657d26f1c5783e5522039e1f7a700
amateur-de <danialcs606@gmail.com>
2024-07-25 22:43:35 +0500

    Completed basic functionality of calculator

---
+++ app.js
@@ -0,0 +1,119 @@
+const add = (a,b)=>a + b
+
+const subtract = (a,b)=>a - b
+
+const multiply = (a,b)=>a * b
+
+const divide = (a,b)=>a / b
+
+const display = document.querySelector('.display')
+
+let firstNumber = 0 , secondNumber = null , operator = null 
+let mode = 'operand1'
+
+const operate = (firstNum , secondNum , operation)=> {
+    if( operation === '+')
+    {
+        return add(Number(firstNum) , Number(secondNum))
+    }
+    else if( operation === '-')
+    {
+        return subtract(firstNum , secondNum)
+    }
+    else if( operation === '*')
+    {
+        return multiply(firstNum , secondNum)
+    }
+    else
+    {
+        return divide(firstNum , secondNum)
+    }
+}
+
+
+
+const displayControl = (e)=> {
+    console.log('Expression class')
+    console.log( e.target.textContent , 'Button')
+    let input = e.target.textContent
+    if( mode === 'operand1')
+    {
+            if( input >= 0 && input <= 9)
+            {
+                firstNumber == 0 ? firstNumber = input : firstNumber += input
+                display.textContent = firstNumber
+            }
+            else if(input === '+' || input === '-' || input === '*' || input === '/')
+            {
+                operator = input
+                mode = 'operand2'
+            }
+    }
+    else if( mode === 'operand2')
+    {
+            if( input >= 0 && input <= 9)
+            {
+                secondNumber == 0 || secondNumber === null
+                 ? secondNumber = input : secondNumber += input
+                display.textContent = secondNumber
+            }
+            else if(input === '+' || input === '-' || input === '*' || input === '/')
+            {
+                if(secondNumber !== null)  
+                {
+                    firstNumber = operate(firstNumber , secondNumber , operator)
+                    display.textContent = firstNumber
+                    operator = input
+                    secondNumber = null
+
+                }
+                
+            }
+    }
+
+    
+
+    
+    
+
+}
+
+
+const buttons = document.querySelectorAll('.expression')
+
+const clear = document.querySelector('.clear')
+
+const result = document.querySelector('.result')
+
+
+
+buttons.forEach((button)=> button.addEventListener('click' , displayControl))
+
+clear.addEventListener('click' , ()=> {
+     firstNumber = 0
+     secondNumber = null
+     operator = null
+     display.textContent = 0
+     mode = 'operand1'
+     
+})
+
+result.addEventListener('click' , ()=> {
+    if( firstNumber !== null && secondNumber !== null && operator !== null)
+    {
+        firstNumber = operate(firstNumber , secondNumber , operator)
+        display.textContent = firstNumber
+        operator = null
+        secondNumber = null
+        mode = 'operand1'
+
+    }
+
+})
+
+
+
+
+
+
+
